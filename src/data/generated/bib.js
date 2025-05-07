d5a80a752bd1cd950e13bc7bf5c784d5d607d6be
ZhenzhuYin <1159480403@qq.com>
2025-05-07 14:41:50 +0800

    更新bib

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,110 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
+    "amirian2025state": {
+        "author": "Amirian, Soheyla and Gao, Fengyi and Littlefield, Nickolas and Hill, Jonathan H and Yates, Adolph J and Plate, Johannes F and Pantanowitz, Liron and Rashidi, Hooman H and Tafti, Ahmad P",
+        "journal": "IEEE Access",
+        "keywords": "type:Responsible AI, Explainable AI, Fair AI, Medical Image Analysis, Ethical AI",
         "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "title": "State-of-the-art in responsible, explainable, and fair ai for medical image analysis",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "url": "https://doi.org/10.1109/ACCESS.2025.3555543",
+        "year": "2025"
+    },
+    "holzinger2019imaging": {
+        "author": "Holzinger, Andreas and Haibe-Kains, Benjamin and Jurisica, Igor",
+        "journal": "European Journal of Nuclear Medicine and Molecular Imaging",
+        "keywords": "type:Imaging Data, AI Integration, Omics Data, Clinical Data, Multimodal Data",
+        "number": "13",
+        "pages": "2722--2730",
+        "publisher": "Springer",
+        "title": "Why imaging data alone is not enough: AI-based integration of imaging, omics, and clinical data",
+        "type": "article",
+        "url": "https://doi.org/10.1007/s00259-019-04382-9",
+        "volume": "46",
+        "year": "2019"
+    },
+    "li2024role": {
+        "author": "Li, Xin and Zhang, Lei and Yang, Jingsi and Teng, Fei",
+        "journal": "Journal of Medical and Biological Engineering",
+        "keywords": "type:Artificial intelligence, Diagnostic imaging, Deep learning, Multi-modal image fusion, Personalized medicine",
+        "number": "2",
+        "pages": "231--243",
+        "publisher": "Springer",
+        "title": "Role of artificial intelligence in medical image analysis: A review of current trends and future directions",
+        "type": "article",
+        "url": "https://doi.org/10.1007/s40846-024-00863-x",
+        "volume": "44",
+        "year": "2024"
+    },
+    "lin2022novel": {
+        "author": "Lin, Qiao and Chen, Xin and Chen, Chao and Garibaldi, Jonathan M",
+        "journal": "IEEE Transactions on Fuzzy Systems",
+        "keywords": "type:Quality Control, Medical Image Segmentation, Fuzzy Uncertainty, Algorithm Development, Image Processing",
+        "number": "8",
+        "pages": "2532--2544",
+        "publisher": "IEEE",
+        "title": "A novel quality control algorithm for medical image segmentation based on fuzzy uncertainty",
+        "type": "article",
+        "url": "https://ieeexplore.ieee.org/stamp/stamp.jsp?tp",
+        "volume": "31",
+        "year": "2022"
+    },
+    "lin2022quality": {
+        "author": "Lin, Qiao and Chen, Xin and Chen, Chao and Garibaldi, Jonathan M",
+        "booktitle": "2022 IEEE International Conference on Fuzzy Systems (FUZZ-IEEE)",
+        "keywords": "type:Deep Convolutional Networks, Quality Quantification, Image Analysis, Machine Learning, Neural Networks",
+        "organization": "IEEE",
+        "pages": "1--8",
+        "title": "Quality quantification in deep convolutional neural networks for skin lesion segmentation using fuzzy uncertainty measurement",
+        "type": "inproceedings",
+        "year": "2022"
+    },
+    "magherini2024development": {
+        "author": "Magherini, Roberto and others",
+        "keywords": "type:Artificial Intelligence Systems, Biomedical Applications, Healthcare Technology, Machine Learning, Data Analysis",
+        "title": "Development of artificial intelligence based systems for biomedical applications",
+        "type": "article",
+        "url": "https://flore.unifi.it/handle/2158/1358371?mode",
+        "year": "2024"
+    },
+    "obuchowicz2024clinical": {
+        "author": "Obuchowicz, Rafa{\\l} and Strzelecki, Micha{\\l} and Pi{\\'o}rkowski, Adam",
+        "journal": "Cancers",
+        "keywords": "type:Artificial Intelligence, Medical Imaging, Image Processing, Deep Learning, Diagnostic Applications",
+        "number": "10",
+        "pages": "1870",
+        "publisher": "MDPI",
+        "title": "Clinical applications of artificial intelligence in medical imaging and image processing\u2014A review",
+        "type": "misc",
+        "url": "https://doi.org/10.3390/cancers16101870",
+        "volume": "16",
+        "year": "2024"
+    },
+    "seo2023applications": {
+        "author": "Seo, Seongmin and Lee, Jai Woo",
+        "booktitle": "Computational Drug Discovery and Design",
+        "keywords": "type:Big Data, AI-Driven Technologies, Computer-Aided Drug Design, Virtual Screening, Drug Discovery",
+        "pages": "295--305",
+        "publisher": "Springer",
+        "title": "Applications of big data and AI-driven technologies in CADD (computer-aided drug design)",
+        "type": "incollection",
+        "url": "https://doi.org/10.1007/978-1-0716-3441-7",
+        "year": "2023"
+    },
+    "wagner2025exploring": {
+        "author": "Wagner, Patrick",
+        "keywords": "type:Artificial Intelligence Lifecycle, AI Development, Technology Lifecycle, Machine Learning Lifecycle, AI Ethics",
+        "title": "Exploring the Lifecycle of Artificial Intelligence--Deep Learning for 1D to 4D Biomedical Data",
+        "type": "article",
+        "url": "https://depositonce.tu-berlin.de/bitstreams/3dd4a9aa-bc60-49f8-a202-1a58d457b695/download",
+        "year": "2025"
+    },
+    "wang2024survey": {
+        "author": "Wang, Chong and Li, Mengyao and He, Junjun and Wang, Zhongruo and Darzi, Erfan and Chen, Zan and Ye, Jin and Li, Tianbin and Su, Yanzhou and Ke, Jing and others",
+        "journal": "arXiv preprint arXiv:2409.00133",
+        "keywords": "type:Large Language Models, Biomedicine, Natural Language Processing, Medical Applications, Healthcare AI",
+        "title": "A survey for large language models in biomedicine",
+        "type": "article",
+        "url": "https://doi.org/10.1007/s12072-023-10630-w",
+        "year": "2024"
     }
 };
